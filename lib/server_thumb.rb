d3ef258fc10ef77eec34ffb7432ca2115e33e51e
Raj Shah <brainix@gmail.com>
2012-10-15 19:53:54 -0700

    implemented server to server Facebook Like

--- lib/server_thumb.rb
+++ lib/server_thumb.rb
@@ -1,1 +1,32 @@
 #!/usr/bin/env ruby
+
+
+
+require 'json'
+require 'net/http'
+require 'URI'
+
+
+
+module ServerThumb
+
+  FACEBOOK_GRAPH_BASE_URL = 'https://graph.facebook.com/'
+
+  def self.like(access_token, object_id)
+    url = FACEBOOK_GRAPH_BASE_URL + object_id + '/og.likes'
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+    request = Net::HTTP::Post.new(uri.path)
+    data = { access_token: access_token, object: object_id }
+    request.set_form_data(data)
+
+    begin
+      response = http.request(request)
+    rescue OpenURI::HTTPError
+      # The user has already liked the Facebook object.
+    end
+    decoded_json = JSON.parse(response.body)
+  end
+
+end
